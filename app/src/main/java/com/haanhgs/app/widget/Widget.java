60b5ecd64e9dba58861afbf2edae1e6b0c58239d
animevn <haanhth21@gmail.com>
2020-03-29 08:53:23 +0700

    must resort to setexact , setinexactrepeating update is very crazy

--- app/src/main/java/com/haanhgs/app/widget/Widget.java
+++ app/src/main/java/com/haanhgs/app/widget/Widget.java
@@ -7,6 +7,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.SystemClock;
 import android.widget.RemoteViews;
 import java.text.DateFormat;
 import java.util.Date;
@@ -46,20 +47,44 @@ public class Widget extends AppWidgetProvider {
         views.setOnClickPendingIntent(R.id.bnRefresh, pendingIntent);
 
         AlarmManager manager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        long timeInMillis = System.currentTimeMillis() + 60000;
+
+//        this is fore exact wakeup after 15 minutes
+        long timeInMillis = System.currentTimeMillis() + AlarmManager.INTERVAL_FIFTEEN_MINUTES;
         if (manager != null){
             manager.setExact(AlarmManager.RTC_WAKEUP, timeInMillis, pendingIntent);
         }
+
+//        //or can set inexactrepeating with this , but need to cancel alarm each time in onUpdat
+//        //working but very weird, seems that setexact should be used here
+//        long interval = AlarmManager.INTERVAL_FIFTEEN_MINUTES/5;
+//        long trigger = System.currentTimeMillis();
+//        if (manager != null){
+//            manager.setInexactRepeating(
+//                    AlarmManager.RTC_WAKEUP, trigger, interval, pendingIntent);
+//        }
 
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+//        //this is for inexact alarm - canceling alarm each time update
+//        AlarmManager manager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+//        for (int appWidgetId : appWidgetIds){
+//            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, appWidgetId,
+//                    new Intent(context, Widget.class), PendingIntent.FLAG_NO_CREATE);
+//            if (manager != null && pendingIntent != null){
+//                manager.cancel(pendingIntent);
+//                pendingIntent.cancel();
+//            }
+//        }
+
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
+
+
 
     @Override public void onEnabled(Context context) {}
     @Override public void onDisabled(Context context) {}
